c6a16162a838e144ef18c8709f2503dab7d42967
howell <howelldann214@gmail.com>
2025-08-09 23:30:04 +0800

    Initial commit for ticket_bot project

---
+++ start-brave.bat
@@ -0,0 +1,6 @@
+@echo off
+start "" "C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe" ^
+  --remote-debugging-port=9222 ^
+  --user-data-dir=%TEMP%\brave-profile ^
+  --no-first-run ^
+  --no-default-browser-check
